3c439b5976513f100d74effd908df2bb85212630
Mitsrael Souza <mitarael9@gmail.com>
2025-05-12 10:43:21 -0300

    Add PM2

--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
 require("express-async-errors")
+require("dotenv/config")
+
 const cors = require("cors")
 const express = require("express")
 const routes = require("./routes")
@@ -37,5 +39,5 @@ app.use((error, req, res, next) => {
     })
 })
 
-const PORT = 3333
+const PORT = process.env.PORT || 3333
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`))
